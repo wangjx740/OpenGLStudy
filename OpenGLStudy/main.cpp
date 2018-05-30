d13657725502c5fbe7f2f28eb0d5b18aea4bb2d6
wangjx740 <173716019@qq.com>
2018-05-30 11:37:28 +0800

    readme and scaler transform

--- OpenGLStudy/main.cpp
+++ OpenGLStudy/main.cpp
@@ -196,7 +196,7 @@ int main()
 		transform = glm::translate(transform, glm::vec3(-0.5f, 0.5f, 0.0f));
 		float scaleAmount = sin(glfwGetTime());
 		transform = glm::scale(transform, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, &transform[0][0]);
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
 		// render container
 		ourShader.use();
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 LearnOpenGL
 book address:
 https://learnopengl-cn.github.io
+【官方双语/合集】线性代数的本质 - 系列合集 理解变换的本质
+https://www.bilibili.com/video/av6731067
